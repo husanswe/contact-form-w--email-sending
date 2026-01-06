a55f2f5ce44603cca133a22150b5357558540d79
husanswe <sulaymonovhusan2002@gmail.com>
2026-01-06 09:11:29 +0500

    Update comment for Bootstrap JS bundle in index.php

--- index.php
+++ index.php
@@ -75,7 +75,7 @@
         </div>
     </div>
 
-    <!-- Bootstrap 5 JS Bundle (includes Popper) -->
+    <!-- Bootstrap 5 JS Bundle -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
